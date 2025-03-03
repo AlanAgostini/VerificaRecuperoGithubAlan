ef3e8e5eb4d80d6c263dacc9ff924705a222d094
Alan Agostini <ALAN.AGOSTINI@local.ittcesena.it>
2025-03-03 09:17:41 +0100

    finita lavorazione sulla classe libro v1.1

--- WpfAppVerificaRecupero/WpfAppVerificaRecupero/Libro.cs
+++ WpfAppVerificaRecupero/WpfAppVerificaRecupero/Libro.cs
@@ -8,5 +8,37 @@ namespace WpfAppVerificaRecupero
 {
     internal class Libro
     {
+        public Libro() 
+        {
+            string CasaEditrice;
+            int Pagine;
+            string Titolo;
+            List<Partita> ListaPartite = new List<Partita>();
+            static List<Partita> RicercaAnno(int anno)
+            {
+                List<Partita> soluzione = new List<Partita>();
+                for (int i = 0; i < ListaPartite.Count; i++)
+                {
+                    if (ListaPartite[i].Anno == anno)
+                    {
+                        soluzione.Add(ListaPartite[i]);
+                    }
+                }
+                return soluzione;
+            }
+            static void AggiungiPartita(Partita partita)
+            {
+                ListaPartite.Add(partita);
+            }
+            static List<Partita> RicercaSquadra()
+            {
+                List<Partita> soluzione = new List<Partita>();
+                return soluzione;
+            }
+            static int totalePartite()
+            {
+                return 0;
+            }
+        }
     }
 }
